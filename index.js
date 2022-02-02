c5cafb68fc52fec0c0676533ea641aa150f6b931
ledelz <97308367+ledelz@users.noreply.github.com>
2022-02-02 13:51:45 -0500

    V-final

---
+++ index.js
@@ -0,0 +1,28 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function (reg) {
+    // registration worked
+    console.log('Registration succeeded.' + reg.scope);
+  }).catch(function (error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+};
+
+// ajout d'une fenetre qui demande à l'utilisateur s'il veut installer l'application;
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  showInstall();
+
+  console.log(`'beforeinstallprompt'`);
+  install.addEventListener('click', function () {
+    deferredPrompt.prompt();
+  })
+});
+
+function showInstall() {
+  const installation = document.getElementById('fenetre')
+  const toast = new bootstrap.Toast(installation, { delay: 10000 })
+  toast.show();
+}
---
+++ sw.js
@@ -0,0 +1,53 @@
+self.addEventListener('install', function (event) {
+  console.log('Installation du service worker ');
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        'favicon.ico',
+        'images/2021-volkswagen-jetta-mmp-1-1597767712.jpg',
+        'images/2021-honda-civic-mmp-1-1595005323.jpg',
+        'images/2021_corolla-apex_exterior_0071614281777066.jpg',
+        'icons-1.7.2/font/bootstrap-icons.css',
+        'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+        'bootstrap-5.1.3-dist/css/bootstrap.min.css',
+        'index.js',
+        'index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log("Fetching ..." + event.request.url);
+  event.respondWith(caches.match(event.request).then((response) => {
+    if (response !== undefined) {
+      return response;
+    } else {
+      console.log("Fetching from fetch ..." + event.request.url);
+      return fetch(event.request);
+    }
+  }))
+})
+
+// possibilité de cloner la requete avec le code suivant : 
+/*
+self.addEventListener('fetch', function(event) {
+    event.respondWith(caches.match(event.request).then(function(response) {
+
+      if (response !== undefined) {
+        return response;
+      } else {
+        console.log("Fetching from fetch ..." + event.request.url);
+        return fetch(event.request).then(function (response) {
+     
+          let responseClone = response.clone();
+          
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        })
+      }
+    }));
+  });
+*/
