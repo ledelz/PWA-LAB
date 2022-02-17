7ff0a3af2e3ae81edef89a5eefd49ff34e297bb4
ledelz <97308367+ledelz@users.noreply.github.com>
2022-02-16 22:18:35 -0500

    okk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('sw.js').then(function (reg) {
     // registration worked
     console.log('Registration succeeded.' + reg.scope);
@@ -31,7 +31,7 @@ function showInstall() {
 }
 
 
-// notifications
+
 function meNotifier() {
   Notification.requestPermission().then(function (result) {
       console.log("permission donnée");
@@ -43,59 +43,3 @@ if ('serviceWorker' in navigator && 'SyncManager' in window) {
       return reg.sync.register('notif');
   });
 };
- */
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("sw.js")
-    .then(function (reg) {
-      console.log("Scope" + reg.scope);
-    })
-    .catch(function (error) {
-      console.log("Error" + error);
-    });
-}
-
-// Initialise la variable
-let deferredPrompt;
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  // pas de barre si installé
-  e.preventDefault();
-  // Réserve evenement
-  deferredPrompt = e;
-  // installer pwa fenêtre avertissement
-  installApp();
-
-  console.log(`'beforeinstallprompt' fired.`);
-
-  // bouton installer
- // appButton.addEventListener("click", function () {
-   // deferredPrompt.prompt();
- // })
-});
-
-// informer l'utilisateur de la possibilité d'installer pwa
-
-function installApp() {
-  console.log("fonctionne");
-  const fenetre = document.getElementById("mytoast");
-  const toast = new bootstrap.Toast(fenetre, {
-    delay: 5000
-  }); //affichée 5 secondes
-  toast.show();
-};
-
-// Demande la permission d'afficher des notifications en cliquant sur le boutton
-function meNotifier() {
-  Notification.requestPermission().then(function (result) {
-    console.log("permission accordée");
-  });
-}
-
-// Déclenche l’enregistrement d’un background sync
-if ("serviceWorker" in navigator && "SyncManager" in window) {
-  navigator.serviceWorker.ready.then(function (reg) {
-    return reg.sync.register("sit");
-  });
-}
--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-/* this.addEventListener('install', function (event) {
+this.addEventListener('install', function (event) {
   console.log('Installation du service worker ');
   event.waitUntil(
     caches.open('v1').then(function (cache) {
@@ -18,6 +17,7 @@
     })
   );
 });
+
 
 this.addEventListener('activate', (e) => {
   console.log('Service worker: active');
@@ -66,78 +66,5 @@ function envoyerNotification() {
       self.registration.showNotification('connexion rétablie avec succes', options);
   } else {
       console.log("aucune notification car non permis");
-  }
-}
- */
-
-this.addEventListener("install", function (event) {
-  console.log("[Service Worker] Installation de v1");
-  event.waitUntil(
-    caches.open("v1").then(function (cache) {
-      console.log("[Service Worker] Mise en cache");
-      return cache.addAll([
-        'favicon.ico',
-        'images/2021-volkswagen-jetta-mmp-1-1597767712.jpg',
-        'images/2021-honda-civic-mmp-1-1595005323.jpg',
-        'images/2021_corolla-apex_exterior_0071614281777066.jpg',
-        'icons-1.7.2/font/bootstrap-icons.css',
-        'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
-        'bootstrap-5.1.3-dist/css/bootstrap.min.css',
-        'index.js',
-        'index.html',
-        'horsLigne.html'
-      ]);
-    })
-  );
-});
-
-
-this.addEventListener("activate", (e) => {
-  console.log("Sw actif");
-});
-
-function cacheOrNetwork(request) {
-  return fromCache(request).catch(() => fetch(request));
-}
-
-function fromCache(request) {
-  return caches.open("v1").then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject("no-match");
-    });
-  });
-}
-
-this.addEventListener("fetch", function (event) {
-  event.respondWith(
-    cacheOrNetwork(event.request).catch(() => fallbackVersPageHorsLigne())
-  );
-});
-
-function fallbackVersPageHorsLigne() {
-  return caches.match("page-hors-ligne.html");
-}
-
-// sync service worker.
-this.addEventListener("sync", function (event) {
-  console.log("reçu : " + event);
-  if (event.tag == "sit") {
-    console.log("Connection réétablie envoie notif si permis");
-    event.waitUntil(envoyerNotification());
-  }
-});
-
-// Connection rétablie. notification: la page est dispo.
-function envoyerNotification() {
-  console.log("Notification envoyée");
-  if (Notification.permission === "granted") {
-    var options = {
-      body: "Page non disponible",
-      requireInteraction: true,
-    };
-
-    self.registration.showNotification("Connection rétabli avec succes", options);
-  } else {
-    console.log("Pas de notif: non permis");
   }
 }
